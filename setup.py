36e704010efb4d8dbd33ea9409ab67fac77f6564
Er.jyoti munde <128744658+yantraved@users.noreply.github.com>
2023-07-29 17:04:26 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
+HYPEN_E_DOT = '-e.'
 
+def get_requirements(file_path):
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+setup(
+    name = "newproject",
+    version = "0.0.1",
+    author = "jyoti",
+    author_email="gjyotimunde@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    )
